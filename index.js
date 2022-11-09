7fca7fede79fe98c62a94743afb67bf36a43b79b
Sujoy Paul <freelancersujoy@gmail.com>
2022-11-09 20:10:04 +0600

    reviews collection created

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run(){
   try{
     const serviceCollection = client.db('travellian').collection('services');
+    const reviewCollection = client.db('travellian').collection('reviews');
 
     app.get('/services', async(req, res) => {
       const query = {};
@@ -39,7 +40,7 @@ async function run(){
       const query = { _id: ObjectId(id) }
       const service = await serviceCollection.findOne(query);
       res.send(service)
-    })
+    });
   }
   finally{
     
